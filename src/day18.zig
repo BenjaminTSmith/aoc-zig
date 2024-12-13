fdd0c4470ddf8529b8f901f562c3dbbde0ea7649
BenjaminTSmith <smithbenjamin@ufl.edu>
2024-12-13 00:55:52 -0500

    day9

--- src/day18.zig
+++ src/day18.zig
@@ -1,12 +1,19 @@
 const std = @import("std");
 
+fn part1() void {}
+
+fn part2() void {}
+
 pub fn main() !void {
     const file = try std.fs.cwd().openFile("input.txt", .{});
     var buffer: [128]u8 = undefined;
-    while (file.reader().readUntilDelimiterOrEof(&buffer, '\n')) |line| {
-        const input = line orelse break;
-        std.debug.print("{s}\n", .{input});
+    while (file.reader().readUntilDelimiterOrEof(&buffer, '\n')) |input| {
+        const line = input orelse break;
+        std.debug.print("{s}\n", .{line});
     } else |err| {
         return err;
     }
+
+    part1();
+    part2();
 }
--- src/day9.zig
+++ src/day9.zig
@@ -6,36 +6,73 @@ pub fn main() !void {
     var line: []u8 = undefined;
 
     var alloc = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer alloc.deinit();
     while (file.reader().readUntilDelimiterOrEof(&buffer, '\n')) |input| {
         line = input orelse break;
     } else |err| {
         return err;
     }
 
-    var ids = std.ArrayList(u32).init(alloc.allocator());
-    var gaps = std.ArrayList(u32).init(alloc.allocator());
-    var num_splits = std.ArrayList(u32).init(alloc.allocator());
+    const File = struct {
+        id: u32,
+        size: u32,
+        index: u32,
+    };
 
+    var disk = std.ArrayList(File).init(alloc.allocator());
+
+    var index: u32 = 0;
     var i: u32 = 0;
     while (i < line.len) : (i += 1) {
+        const size = try std.fmt.parseInt(u32, line[i .. i + 1], 10);
         if (i % 2 == 0) {
-            try num_splits.append(0);
+            try disk.append(.{
+                .id = @intCast(i / 2),
+                .size = size,
+                .index = index,
+            });
         } else {
-            try gaps.append(try std.fmt.parseInt(u32, line[i .. i + 1], 10));
+            try disk.append(.{
+                .id = 0,
+                .size = size,
+                .index = index,
+            });
         }
-    }
-
-    i = 0;
-    while (i < line.len) : (i += 1) {
-        var j: u32 = @intCast(line.len - 1);
-        while (j > 0) : (j -= 2) {
-
-        }
+        index += size;
     }
 
     var checksum: u64 = 0;
-    for (ids.items, 0..) |id, idx| {
-        checksum += id * idx;
+    i = @intCast(disk.items.len - 1);
+    while (i >= 1) : (i -= 1) {
+        var current_file = disk.items[i];
+        if (current_file.id == 0) {
+            continue;
+        }
+        var j: u32 = 1;
+        var found: bool = false;
+        while (j < i) : (j += 1) {
+            if (disk.items[j].id == 0) {
+                if (disk.items[j].size >= current_file.size) {
+                    found = true;
+                    break;
+                }
+            }
+        }
+        if (found) {
+            current_file.index = disk.items[j].index;
+            disk.items[j].index += current_file.size;
+            disk.items[j].size -= current_file.size;
+            var k: u32 = current_file.index;
+            while (k < current_file.index + current_file.size) : (k += 1) {
+                checksum += k * current_file.id;
+            }
+        } else {
+            var k: u32 = current_file.index;
+            while (k < current_file.index + current_file.size) : (k += 1) {
+                checksum += k * current_file.id;
+            }
+        }
     }
+
     std.debug.print("{}\n", .{checksum});
 }
