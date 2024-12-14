31205b2098c829d20d93023b24ef19f509ad0580
BenjaminTSmith <smithbenjamin@ufl.edu>
2024-12-14 00:59:51 -0500

    day 11

--- src/day10.zig
+++ src/day10.zig
@@ -1,19 +1,168 @@
 const std = @import("std");
 
-fn part1() void {}
+fn pathfind(x: u32, y: u32, map: std.ArrayList([]u8)) !u32 {
+    if (map.items[y][x] == '9') {
+        return 1;
+    }
 
-fn part2() void {}
+    var score: u32 = 0;
+    if (x != 0) {
+        const weight = try std.fmt.parseInt(u32, map.items[y][x .. x + 1], 10);
+        const next_weight = try std.fmt.parseInt(u32, map.items[y][x - 1 .. x], 10);
+        if (next_weight == weight + 1) {
+            score += try pathfind(x - 1, y, map);
+        }
+    }
+    if (x + 1 < map.items[0].len) {
+        const weight = try std.fmt.parseInt(u32, map.items[y][x .. x + 1], 10);
+        const next_weight = try std.fmt.parseInt(u32, map.items[y][x + 1 .. x + 2], 10);
+        if (next_weight == weight + 1) {
+            score += try pathfind(x + 1, y, map);
+        }
+    }
+    if (y != 0) {
+        const weight = try std.fmt.parseInt(u32, map.items[y][x .. x + 1], 10);
+        const next_weight = try std.fmt.parseInt(u32, map.items[y - 1][x .. x + 1], 10);
+        if (next_weight == weight + 1) {
+            score += try pathfind(x, y - 1, map);
+        }
+    }
+    if (y + 1 < map.items.len) {
+        const weight = try std.fmt.parseInt(u32, map.items[y][x .. x + 1], 10);
+        const next_weight = try std.fmt.parseInt(u32, map.items[y + 1][x .. x + 1], 10);
+        if (next_weight == weight + 1) {
+            score += try pathfind(x, y + 1, map);
+        }
+    }
+    return score;
+}
 
-pub fn main() !void {
+fn dfs(x: u32, y: u32, map: std.ArrayList([]u8), visited: *std.ArrayList([]bool)) !u32 {
+    visited.items[y][x] = true;
+
+    if (map.items[y][x] == '9') {
+        return 1;
+    }
+
+    var score: u32 = 0;
+    if (x != 0 and !visited.items[y][x - 1]) {
+        const weight = try std.fmt.parseInt(u32, map.items[y][x .. x + 1], 10);
+        const next_weight = try std.fmt.parseInt(u32, map.items[y][x - 1 .. x], 10);
+        if (next_weight == weight + 1) {
+            score += try dfs(x - 1, y, map, visited);
+        }
+    }
+    if (x + 1 < map.items[0].len and !visited.items[y][x + 1]) {
+        const weight = try std.fmt.parseInt(u32, map.items[y][x .. x + 1], 10);
+        const next_weight = try std.fmt.parseInt(u32, map.items[y][x + 1 .. x + 2], 10);
+        if (next_weight == weight + 1) {
+            score += try dfs(x + 1, y, map, visited);
+        }
+    }
+    if (y != 0 and !visited.items[y - 1][x]) {
+        const weight = try std.fmt.parseInt(u32, map.items[y][x .. x + 1], 10);
+        const next_weight = try std.fmt.parseInt(u32, map.items[y - 1][x .. x + 1], 10);
+        if (next_weight == weight + 1) {
+            score += try dfs(x, y - 1, map, visited);
+        }
+    }
+    if (y + 1 < map.items.len and !visited.items[y + 1][x]) {
+        const weight = try std.fmt.parseInt(u32, map.items[y][x .. x + 1], 10);
+        const next_weight = try std.fmt.parseInt(u32, map.items[y + 1][x .. x + 1], 10);
+        if (next_weight == weight + 1) {
+            score += try dfs(x, y + 1, map, visited);
+        }
+    }
+    return score;
+}
+
+fn part1() !void {
+    var alloc = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer alloc.deinit();
+    var map = std.ArrayList([]u8).init(alloc.allocator());
+
     const file = try std.fs.cwd().openFile("input.txt", .{});
     var buffer: [128]u8 = undefined;
     while (file.reader().readUntilDelimiterOrEof(&buffer, '\n')) |input| {
         const line = input orelse break;
-        std.debug.print("{s}\n", .{line});
+        var copy = try alloc.allocator().alloc(u8, line.len);
+        for (line, 0..) |char, i| {
+            copy[i] = char;
+        }
+        try map.append(copy);
     } else |err| {
         return err;
     }
 
-    part1();
-    part2();
+    var visited = std.ArrayList([]bool).init(alloc.allocator());
+    for (map.items) |row| {
+        var visited_row = try alloc.allocator().alloc(bool, row.len);
+        for (row, 0..) |_, i| {
+            visited_row[i] = false;
+        }
+        try visited.append(visited_row);
+    }
+
+    var score: u32 = 0;
+    for (map.items, 0..) |row, y| {
+        for (row, 0..) |_, x| {
+            if (map.items[y][x] == '0') {
+                score += try dfs(@intCast(x), @intCast(y), map, &visited);
+            }
+            for (visited.items) |visited_row| {
+                for (visited_row) |*visit| {
+                    visit.* = false;
+                }
+            }
+        }
+    }
+    std.debug.print("{}\n", .{score});
+}
+
+fn part2() !void {
+    var alloc = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer alloc.deinit();
+    var map = std.ArrayList([]u8).init(alloc.allocator());
+
+    const file = try std.fs.cwd().openFile("input.txt", .{});
+    var buffer: [128]u8 = undefined;
+    while (file.reader().readUntilDelimiterOrEof(&buffer, '\n')) |input| {
+        const line = input orelse break;
+        var copy = try alloc.allocator().alloc(u8, line.len);
+        for (line, 0..) |char, i| {
+            copy[i] = char;
+        }
+        try map.append(copy);
+    } else |err| {
+        return err;
+    }
+
+    var visited = std.ArrayList([]bool).init(alloc.allocator());
+    for (map.items) |row| {
+        var visited_row = try alloc.allocator().alloc(bool, row.len);
+        for (row, 0..) |_, i| {
+            visited_row[i] = false;
+        }
+        try visited.append(visited_row);
+    }
+
+    var score: u32 = 0;
+    for (map.items, 0..) |row, y| {
+        for (row, 0..) |_, x| {
+            if (map.items[y][x] == '0') {
+                score += try pathfind(@intCast(x), @intCast(y), map);
+            }
+            for (visited.items) |visited_row| {
+                for (visited_row) |*visit| {
+                    visit.* = false;
+                }
+            }
+        }
+    }
+    std.debug.print("{}\n", .{score});
+}
+
+pub fn main() !void {
+    try part1();
+    try part2();
 }
--- src/day11.zig
+++ src/day11.zig
@@ -1,19 +1,86 @@
 const std = @import("std");
 
-fn part1() void {}
+const Key = struct {
+    stone: u128,
+    blinks: u64,
+};
 
-fn part2() void {}
-
-pub fn main() !void {
-    const file = try std.fs.cwd().openFile("input.txt", .{});
-    var buffer: [128]u8 = undefined;
-    while (file.reader().readUntilDelimiterOrEof(&buffer, '\n')) |input| {
-        const line = input orelse break;
-        std.debug.print("{s}\n", .{line});
-    } else |err| {
-        return err;
+fn blink_with_memo(stone: u128, blinks: u64, memo: *std.AutoHashMap(Key, u64)) !u64 {
+    if (memo.get(.{ .stone = stone, .blinks = blinks })) |result| {
+        return result;
     }
 
-    part1();
-    part2();
+    if (blinks == 75) {
+        return 1;
+    }
+
+    var buffer: [128]u8 = undefined;
+    const stone_str = try std.fmt.bufPrint(&buffer, "{}", .{stone});
+    if (stone_str.len % 2 == 0) {
+        const mid = stone_str.len / 2;
+        const result = try blink_with_memo(try std.fmt.parseInt(u128, stone_str[0..mid], 10), blinks + 1, memo) +
+            try blink_with_memo(try std.fmt.parseInt(u128, stone_str[mid..], 10), blinks + 1, memo);
+        try memo.put(.{ .stone = stone, .blinks = blinks }, result);
+        return result;
+    } else if (stone == 0) {
+        const result = try blink_with_memo(1, blinks + 1, memo);
+        try memo.put(.{ .stone = stone, .blinks = blinks }, result);
+        return result;
+    } else {
+        const result = try blink_with_memo(stone * 2024, blinks + 1, memo);
+        try memo.put(.{ .stone = stone, .blinks = blinks }, result);
+        return result;
+    }
+}
+
+fn blink(stone: u64, blinks: u64) !u32 {
+    if (blinks == 25) {
+        return 1;
+    }
+
+    var buffer: [128]u8 = undefined;
+    const stone_str = try std.fmt.bufPrint(&buffer, "{}", .{stone});
+    if (stone_str.len % 2 == 0) {
+        const mid = stone_str.len / 2;
+        return try blink(try std.fmt.parseInt(u64, stone_str[0..mid], 10), blinks + 1) + try blink(try std.fmt.parseInt(u64, stone_str[mid..], 10), blinks + 1);
+    } else if (stone == 0) {
+        return try blink(1, blinks + 1);
+    } else {
+        return try blink(stone * 2024, blinks + 1);
+    }
+}
+
+fn part1() !void {
+    const file = try std.fs.cwd().openFile("input.txt", .{});
+    var buffer: [128]u8 = undefined;
+    const line: []u8 = try file.reader().readUntilDelimiterOrEof(&buffer, '\n') orelse unreachable;
+    var tokenizer = std.mem.tokenizeAny(u8, line, " ");
+    var stones: u64 = 0;
+    while (tokenizer.next()) |token| {
+        const stone = try std.fmt.parseInt(u64, token, 10);
+        stones += try blink(stone, 0);
+    }
+    std.debug.print("{}\n", .{stones});
+}
+
+fn part2() !void {
+    var alloc = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer alloc.deinit();
+
+    const file = try std.fs.cwd().openFile("input.txt", .{});
+    var buffer: [128]u8 = undefined;
+    const line: []u8 = try file.reader().readUntilDelimiterOrEof(&buffer, '\n') orelse unreachable;
+    var tokenizer = std.mem.tokenizeAny(u8, line, " ");
+    var stones: u64 = 0;
+    var memo = std.AutoHashMap(Key, u64).init(alloc.allocator());
+    while (tokenizer.next()) |token| {
+        const stone = try std.fmt.parseInt(u64, token, 10);
+        stones += try blink_with_memo(stone, 0, &memo);
+    }
+    std.debug.print("{}\n", .{stones});
+}
+
+pub fn main() !void {
+    try part1();
+    try part2();
 }
